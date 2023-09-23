bb3f93fd5c7ea141dfa046e855b30415346ebf3f
Shrishail Patil <000shrishailpatil@gmail.com>
2023-09-23 07:58:17 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
-import app
-app = app()
+import app  # Import the 'app' module
+
+# Create an instance of the WSGI application
+application = app.app()  # Use a different variable name to avoid conflicts
+
+if __name__ == "__main__":
+    application.run()
